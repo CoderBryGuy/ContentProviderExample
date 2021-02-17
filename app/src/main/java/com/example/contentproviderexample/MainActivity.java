a0c655ffad6f0bdb647394585540f560d1dc7145
Bryan <bryan.dov.bergman@gmail.com>
2021-02-17 23:35:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/contentproviderexample/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.contentproviderexample;
+
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ListView contactNames;
+    private static final int REQUEST_CODE_READ_CONTACTS = 1;
+    private static boolean READ_CONTACTS_GRANTED = false;
+    FloatingActionButton fab = null;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: starts");
+        switch(requestCode){
+            case REQUEST_CODE_READ_CONTACTS:{
+                //if request is cancelled, the rest arrays are empty.
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission granted granted, yay! Do the
+                    //contacts-related task you need to do
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    READ_CONTACTS_GRANTED = true;
+                }else{
+                    //permission denied, boo! disable functionality
+                    //that depends on this permission
+                    Log.d(TAG, "onRequestPermissionsResult:  permission refused");
+                }
+                fab.setEnabled(READ_CONTACTS_GRANTED);
+            }
+        }
+        Log.d(TAG, "onRequestPermissionsResult: ends");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        contactNames = (ListView) findViewById(R.id.contact_names);
+        int hasReadContactsPermission = ContextCompat.checkSelfPermission(this, READ_CONTACTS);
+        Log.d(TAG, "onCreate: check self permission  =  " + hasReadContactsPermission);
+
+        if(hasReadContactsPermission == PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "onCreate: permission granted");
+            READ_CONTACTS_GRANTED = true;
+        }else{
+            Log.d(TAG, "onCreate: requesting permission");
+            ActivityCompat.requestPermissions(this, new String[]{READ_CONTACTS}, REQUEST_CODE_READ_CONTACTS);
+        }
+
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "fab onClick:  starts");
+                String[] projection = {ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
+                ContentResolver contentResolver = getContentResolver();
+                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        ContactsContract.Contacts.DISPLAY_NAME);
+
+                if(cursor != null){
+                    List<String> contacts = new ArrayList<>();
+                    while(cursor.moveToNext()){
+                        contacts.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)));
+                    }
+                    cursor.close();
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.contact_detail, R.id.name, contacts);
+                    contactNames.setAdapter(adapter);
+                }
+                Log.d(TAG, "fab onClick: ends");
+            }
+        });
+
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
